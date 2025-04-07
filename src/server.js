f5df47ed402876eeee625a42784a7dbeaf87238d
MThuyet <mthuyet294@gmail.com>
2025-04-07 14:21:39 +0700

    validation with joi

--- src/server.js
+++ src/server.js
@@ -1,33 +1,37 @@
-import express from 'express';
-import exitHook from 'async-exit-hook';
-import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb';
-import { env } from '~/config/environment';
-import { APIs_V1 } from '~/routes/v1';
+import express from 'express'
+import exitHook from 'async-exit-hook'
+import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
+import { env } from '~/config/environment'
+import { APIs_V1 } from '~/routes/v1'
 
 const START_SERVER = () => {
-  const app = express();
+  const app = express()
 
-  app.use('/v1', APIs_V1);
+  // Sử dụng JSON trong request
+  app.use(express.json())
+
+  // Sử dụng API v1
+  app.use('/v1', APIs_V1)
 
   app.listen(env.APP_PORT, env.APP_HOST, () => {
-    console.log(`Hello ${env.AUTHOR}, I am running at http://${env.APP_HOST}:${env.APP_PORT}`);
-  });
+    console.log(`Hello ${env.AUTHOR}, I am running at http://${env.APP_HOST}:${env.APP_PORT}`)
+  })
 
   exitHook(() => {
-    console.log('Closing DB...');
-    CLOSE_DB();
-    console.log('Closed DB successfully');
-  });
-};
+    console.log('Closing DB...')
+    CLOSE_DB()
+    console.log('Closed DB successfully')
+  })
+}
 
-(async () => {
+;(async () => {
   try {
-    console.log('Connecting to DB...');
-    await CONNECT_DB();
-    console.log('Connected to DB successfully');
-    START_SERVER();
+    console.log('Connecting to DB...')
+    await CONNECT_DB()
+    console.log('Connected to DB successfully')
+    START_SERVER()
   } catch (error) {
-    console.error(error);
-    process.exit(0);
+    console.error(error)
+    process.exit(0)
   }
-})();
+})()

904be91190b57b45f8acbf1bba279ce8b14bcfc9
MThuyet <mthuyet294@gmail.com>
2025-05-18 22:20:50 +0700

    definded flow api create card

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { boardRoute } from './boardRoute'
 import { columnRoute } from './columnRoute'
+import { cardRoute } from './cardRoute'
 
 const Router = express.Router()
 
@@ -9,5 +10,8 @@ Router.use('/boards', boardRoute)
 
 // Column API
 Router.use('/columns', columnRoute)
+
+// Card API
+Router.use('/cards', cardRoute)
 
 export const APIs_V1 = Router

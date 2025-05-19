91550912faa533fb59523f9c6e548094f7442e56
MThuyet <mthuyet294@gmail.com>
2025-05-19 14:27:45 +0700

    feat: implement create API for columnOrderIds in board

--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -2,6 +2,7 @@ import { StatusCodes } from 'http-status-codes'
 import Joi from 'joi'
 import ApiError from '~/utils/ApiError'
 import { BOARD_TYPES } from '~/utils/constants'
+import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators'
 
 const createNew = async (req, res, next) => {
   /*
@@ -37,6 +38,24 @@ const createNew = async (req, res, next) => {
   }
 }
 
+const updateColumnOrderIds = async (req, res, next) => {
+  const correctCondition = Joi.object({
+    columnOrderIds: Joi.array().items(Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE)).required()
+  })
+
+  try {
+    await correctCondition.validateAsync(req.body, {
+      abortEarly: false,
+      allowUnknown: true // cho phép có thể truyền vào các field không được khai báo trong điều kiện
+    })
+
+    next()
+  } catch (error) {
+    next(new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, new Error(error).message))
+  }
+}
+
 export const boardValidation = {
-  createNew
+  createNew,
+  updateColumnOrderIds
 }
